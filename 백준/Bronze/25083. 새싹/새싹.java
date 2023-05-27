32de4d528b1fc85a28643bcae4743c3750682110
Dongyoung <kjptts2241@gmail.com>
2023-05-27 10:40:31 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("         ,r'\"7").append("\n");
+        sb.append("r`-_   ,'  ,/").append("\n");
+        sb.append(" \\. \". L_r'").append("\n");
+        sb.append("   `~\\/").append("\n");
+        sb.append("      |").append("\n");
+        sb.append("      |").append("\n");
+
+        bw.write(sb + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
